707a6aabb0c6f075ed47d7c9b630cdd5f49fcda3
Syke2good <akashbhandari930@gmail.com>
2022-10-12 01:17:57 -0700

    adding a navbar

--- kamal-palace/src/App.js
+++ kamal-palace/src/App.js
@@ -1,9 +1,27 @@
-import react from 'react'
+import React from 'react'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+import Portfolio from './components/Portfolio'
+import Products from './components/Products'
+import ScrollToTop from './components/ScrollToTop'
+import Services from './components/Services'
+import Testomonials from './components/Testomonials'
+
 
 export default function App() {
   return (
-    <div>
-      hello world
-    </div>
-  )
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Testomonials />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </>
+  );
 }
---
+++ kamal-palace/src/components/Testomonials.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Testomonials() {
+  return (
+    <div></div>
+  )
+}
